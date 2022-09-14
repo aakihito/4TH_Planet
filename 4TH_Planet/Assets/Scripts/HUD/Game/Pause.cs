4bdeb21036df3fd008acedc71e58691b6a8db788
IceShadow2 <110208664+IceShadow2@users.noreply.github.com>
2022-09-14 09:32:48 -0300

    4TH Planet V6.6
    Adição de tela de pause

---
+++ 4TH_Planet/Assets/Scripts/HUD/Game/Pause.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Pause : MonoBehaviour
+{
+
+    private bool isPaused;
+
+    [Header ("Pause")]
+    public GameObject Pause_painel;
+    public GameObject Canvas;
+    public string cena;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            PauseScreen();
+        }
+    }
+
+    void PauseScreen()
+    {
+        if(isPaused)
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+            Pause_painel.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+            Pause_painel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void MenuGame()
+    {
+        GameManager.Instance.SceneLoadManager.LoadScene("Menu");
+    }
+}
